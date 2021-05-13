19b76d4d9d973e4753a58bddc0e923c2f898205c
Soham Gupta <gsoham562@gmail.com>
2021-05-13 17:04:22 +0530

    Added Content to some files

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
-app.use('/css', app.static(path.resolve(__dirname, "assets/css")));
-app.use('/js', app.static(path.resolve(__dirname, "assets/js")));
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
 
 app.get('/' , (req, res) => {
     res.render('index');
+})
+app.get('/update', (req, res) => {
+    res.render('update_user');
+})
+app.get('/add', (req, res) => {
+    res.render('add_user');
 })
 
 app.listen(8080);
