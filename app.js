ff63dd8d97f27644372f6b9d6a4063583db6655f
Soham Gupta <gsoham562@gmail.com>
2021-05-17 13:04:59 +0530

    router error fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const router = require('server/routes/router');
+const router = require('./server/routes/router');
 const path = require('path');
 
 app.set('view engine', 'ejs');
